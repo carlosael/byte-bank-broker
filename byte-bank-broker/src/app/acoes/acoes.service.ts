c5128325cd1df6a4488a9b4ca4d15a114da73132
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-01-06 15:17:51 -0300

    feat: add sort to request

--- byte-bank-broker/src/app/acoes/acoes.service.ts
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -1,5 +1,7 @@
+import { Acao } from './modelo/acoes';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map, pluck } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +10,21 @@ export class AcoesService {
   constructor(private httpClient: HttpClient) {}
 
   getAcoes() {
-    return this.httpClient.get<any>('http://localhost:3000/acoes');
+    return this.httpClient.get<any>('http://localhost:3000/acoes').pipe(
+      pluck('payload'),
+      map((acoes) =>
+        acoes.sort((acaoA, acaoB) => this.ordenaPorCodigo(acaoA, acaoB))
+      )
+    );
+  }
+
+  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao) {
+    if (acaoA.codigo > acaoB.codigo) {
+      return 1;
+    } else if (acaoA.codigo < acaoB.codigo) {
+      return -1;
+    } else {
+      return 0;
+    }
   }
 }
